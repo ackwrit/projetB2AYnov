e16b94cbcc9da09442d8e36694e2a3c7d95e12d6
djinodissingar <dissingardjino@icloud.com>
2022-03-15 11:04:33 +0100

    Mise en palce configuration main

---
+++ lib/functions/FirestoreHelper.dart
@@ -0,0 +1,36 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirestoreHelper{
+  //attributs
+  final auth = FirebaseAuth.instance;
+  final fire_user = FirebaseFirestore.instance.collection("Utilisateurs");
+
+
+  //Constructeur
+
+
+
+  //--------------------Méthode---------------------------------
+
+//Méthode pour l'inscription
+
+  Inscription(){
+
+}
+
+
+
+//Méthode pour la connexion
+
+
+//Méthode pour ajouter un utilisateur
+
+
+
+
+
+
+
+}
